776457fcee21f8294d6c9113519a0c851c1ca3d3
aizatnazran <aizatnazran@gmail.com>
2023-12-11 13:27:02 +0800

    minor integration commit 11/12

--- src/pages/integration.vue
+++ src/pages/integration.vue
@@ -23,45 +23,74 @@ const sheet = ref(false)
 
 const companyId = localStorage.getItem('company_id')
 
-const integrations = ref([
-  {
-    name: 'Zoho',
-    description: 'A unique and powerful software suite for businesses of all sizes.',
-    image: zoho,
-  },
-  {
-    name: 'Clover',
-    description: 'A unique and powerful software suite for businesses of all sizes.',
-    image: clover,
-  },
-  {
-    name: 'StoreHub',
-    description: 'A unique and powerful software suite for businesses of all sizes.',
-    image: storehub,
-  },
-  {
-    name: 'SalesForce',
-    description: 'A unique and powerful software suite for businesses of all sizes.',
-    image: salesforce,
-  },
-  {
-    name: 'MondaySalesCRM',
-    description: 'A unique and powerful software suite for businesses of all sizes.',
-    image: mondaysales,
-  },
-  {
-    name: 'NexCRM',
-    description: 'A unique and powerful software suite for businesses of all sizes.',
-    image: nexcrm,
-  },
-])
+// Categorized integrations
+const integrations = ref({
+  accountingSystem: [
+    {
+      name: 'Zoho',
+      description: 'A unique and powerful software suite for businesses of all sizes.',
+      image: zoho,
+    },
+    {
+      name: 'Clover',
+      description: 'A unique and powerful software suite for businesses of all sizes.',
+      image: clover,
+    },
+    {
+      name: 'StoreHub',
+      description: 'A unique and powerful software suite for businesses of all sizes.',
+      image: storehub,
+    },
+    {
+      name: 'SalesForce',
+      description: 'A unique and powerful software suite for businesses of all sizes.',
+      image: salesforce,
+    },
+    {
+      name: 'MondaySalesCRM',
+      description: 'A unique and powerful software suite for businesses of all sizes.',
+      image: mondaysales,
+    },
+    {
+      name: 'NexCRM',
+      description: 'A unique and powerful software suite for businesses of all sizes.',
+      image: nexcrm,
+    },
+  ],
+  cloudStorage: [
+    {
+      name: 'Zoho',
+      description: 'A unique and powerful software suite for businesses of all sizes.',
+      image: zoho,
+    },
+    {
+      name: 'Clover',
+      description: 'A unique and powerful software suite for businesses of all sizes.',
+      image: clover,
+    },
+    {
+      name: 'StoreHub',
+      description: 'A unique and powerful software suite for businesses of all sizes.',
+      image: storehub,
+    },
+    {
+      name: 'SalesForce',
+      description: 'A unique and powerful software suite for businesses of all sizes.',
+      image: salesforce,
+    },
+  ],
+})
 </script>
 
 <template>
   <v-container fluid>
+    <!-- Accounting System Section -->
     <v-row>
+      <v-col cols="12">
+        <h2>Accounting System</h2>
+      </v-col>
       <v-col
-        v-for="integration in integrations"
+        v-for="integration in integrations.accountingSystem"
         :key="integration.name"
         cols="12"
         sm="6"
@@ -72,7 +101,6 @@ const integrations = ref([
           elevation="2"
         >
           <v-row no-gutters>
-            <!-- Image on the left side -->
             <v-col
               cols="3"
               class="d-flex align-start"
@@ -83,7 +111,6 @@ const integrations = ref([
               ></v-img>
             </v-col>
 
-            <!-- Text and connect button in the middle -->
             <v-col
               cols="7"
               class="d-flex flex-column justify-space-between"
@@ -103,7 +130,77 @@ const integrations = ref([
               </v-btn>
             </v-col>
 
-            <!-- Three dot menu icon on the top right -->
+            <v-col
+              cols="2"
+              class="d-flex justify-end align-start"
+            >
+              <v-menu offset-y>
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    icon
+                    v-on="on"
+                  >
+                    <v-icon>mdi-dots-vertical</v-icon>
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item link>
+                    <!-- Menu content here -->
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <!-- Cloud Storage Section -->
+    <v-row>
+      <v-col cols="12">
+        <h2>Cloud Storage</h2>
+      </v-col>
+      <v-col
+        v-for="integration in integrations.cloudStorage"
+        :key="integration.name"
+        cols="12"
+        sm="6"
+        md="4"
+      >
+        <v-card
+          class="pa-4"
+          elevation="2"
+        >
+          <v-row no-gutters>
+            <v-col
+              cols="3"
+              class="d-flex align-start"
+            >
+              <v-img
+                :src="integration.image"
+                contain
+              ></v-img>
+            </v-col>
+
+            <v-col
+              cols="7"
+              class="d-flex flex-column justify-space-between"
+            >
+              <div>
+                <v-card-title>{{ integration.name }}</v-card-title>
+                <v-card-text>{{ integration.description }}</v-card-text>
+              </div>
+              <v-btn
+                color="primary"
+                text
+                block
+                rounded="xl"
+                @click="toggleConnection(integration)"
+              >
+                Connect
+              </v-btn>
+            </v-col>
+
             <v-col
               cols="2"
               class="d-flex justify-end align-start"
