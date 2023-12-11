9d4dce758b1e140d5a5d8ca64aa49d87a6b60170
aizatnazran <aizatnazran@gmail.com>
2023-12-11 17:15:50 +0800

    11/12 Complete integration page

--- src/pages/integration.vue
+++ src/pages/integration.vue
@@ -7,7 +7,6 @@ import storehub from '@images/logos/storehub.png'
 import zoho from '@images/logos/zoho.png'
 import { ref } from 'vue'
 import { useTheme } from 'vuetify'
-// Components
 
 const vuetifyTheme = useTheme()
 
@@ -33,27 +32,27 @@ const integrations = ref({
     },
     {
       name: 'Clover',
-      description: 'A unique and powerful software suite for businesses of all sizes.',
+      description: 'POS systems that handle the ever-changing parts of your business.',
       image: clover,
     },
     {
       name: 'StoreHub',
-      description: 'A unique and powerful software suite for businesses of all sizes.',
+      description: 'An all-in-one platform that enables retailers automate their businesses.',
       image: storehub,
     },
     {
       name: 'SalesForce',
-      description: 'A unique and powerful software suite for businesses of all sizes.',
+      description: 'Salesforce helps businesses keep track of customer interactions and sales data.',
       image: salesforce,
     },
     {
       name: 'MondaySalesCRM',
-      description: 'A unique and powerful software suite for businesses of all sizes.',
+      description: 'Monday Sales CRM is a',
       image: mondaysales,
     },
     {
       name: 'NexCRM',
-      description: 'A unique and powerful software suite for businesses of all sizes.',
+      description: 'NexCRM is a',
       image: nexcrm,
     },
   ],
@@ -65,17 +64,17 @@ const integrations = ref({
     },
     {
       name: 'Clover',
-      description: 'A unique and powerful software suite for businesses of all sizes.',
+      description: 'POS systems that handle the ever-changing parts of your business.',
       image: clover,
     },
     {
       name: 'StoreHub',
-      description: 'A unique and powerful software suite for businesses of all sizes.',
+      description: 'An all-in-one platform that enables retailers automate their businesses.',
       image: storehub,
     },
     {
       name: 'SalesForce',
-      description: 'A unique and powerful software suite for businesses of all sizes.',
+      description: 'Salesforce helps businesses keep track of customer interactions and sales data.',
       image: salesforce,
     },
   ],
@@ -84,11 +83,28 @@ const integrations = ref({
 
 <template>
   <v-container fluid>
-    <!-- Accounting System Section -->
-    <v-row>
-      <v-col cols="12">
-        <h2>Accounting System</h2>
+    <!-- Accounting System Section Title -->
+    <v-row class="mb-4">
+      <v-col
+        cols="5"
+        class="d-flex align-center"
+      >
+        <v-divider></v-divider>
       </v-col>
+      <v-col
+        cols="2"
+        class="text-center"
+      >
+        <span class="subtitle-1 font-weight-bold">Accounting System</span>
+      </v-col>
+      <v-col
+        cols="5"
+        class="d-flex align-center"
+      >
+        <v-divider></v-divider>
+      </v-col>
+    </v-row>
+    <v-row>
       <v-col
         v-for="integration in integrations.accountingSystem"
         :key="integration.name"
@@ -117,9 +133,10 @@ const integrations = ref({
             >
               <div>
                 <v-card-title>{{ integration.name }}</v-card-title>
-                <v-card-text>{{ integration.description }}</v-card-text>
+                <v-card-text class="fixed-height">{{ integration.description }}</v-card-text>
               </div>
               <v-btn
+                class="connect-btn"
                 color="primary"
                 text
                 block
@@ -138,15 +155,14 @@ const integrations = ref({
                 <template v-slot:activator="{ on }">
                   <v-btn
                     icon
+                    class="dots-button"
                     v-on="on"
                   >
                     <v-icon>mdi-dots-vertical</v-icon>
                   </v-btn>
                 </template>
                 <v-list>
-                  <v-list-item link>
-                    <!-- Menu content here -->
-                  </v-list-item>
+                  <v-list-item link> </v-list-item>
                 </v-list>
               </v-menu>
             </v-col>
@@ -156,10 +172,27 @@ const integrations = ref({
     </v-row>
 
     <!-- Cloud Storage Section -->
-    <v-row>
-      <v-col cols="12">
-        <h2>Cloud Storage</h2>
+    <v-row class="mt-15 mb-4">
+      <v-col
+        cols="5"
+        class="d-flex align-center"
+      >
+        <v-divider></v-divider>
       </v-col>
+      <v-col
+        cols="2"
+        class="text-center"
+      >
+        <span class="subtitle-1 font-weight-bold">Cloud Storage</span>
+      </v-col>
+      <v-col
+        cols="5"
+        class="d-flex align-center"
+      >
+        <v-divider></v-divider>
+      </v-col>
+    </v-row>
+    <v-row>
       <v-col
         v-for="integration in integrations.cloudStorage"
         :key="integration.name"
@@ -188,9 +221,10 @@ const integrations = ref({
             >
               <div>
                 <v-card-title>{{ integration.name }}</v-card-title>
-                <v-card-text>{{ integration.description }}</v-card-text>
+                <v-card-text class="fixed-height">{{ integration.description }}</v-card-text>
               </div>
               <v-btn
+                class="connect-btn"
                 color="primary"
                 text
                 block
@@ -209,15 +243,14 @@ const integrations = ref({
                 <template v-slot:activator="{ on }">
                   <v-btn
                     icon
+                    class="dots-button"
                     v-on="on"
                   >
                     <v-icon>mdi-dots-vertical</v-icon>
                   </v-btn>
                 </template>
                 <v-list>
-                  <v-list-item link>
-                    <!-- Menu content here -->
-                  </v-list-item>
+                  <v-list-item link> </v-list-item>
                 </v-list>
               </v-menu>
             </v-col>
@@ -227,3 +260,19 @@ const integrations = ref({
     </v-row>
   </v-container>
 </template>
+
+<style scoped>
+.connect-btn {
+  margin-top: 2; /* Adjust the margin as needed */
+}
+
+.dots-button {
+  padding: 0px; /* Reduce padding to make the button smaller */
+  font-size: 18px; /* Adjust font size for the icon inside the button */
+}
+
+.fixed-height {
+  height: 100px; /* Or whatever height you find appropriate */
+  overflow: auto; /* To allow scrolling if the text is longer */
+}
+</style>
