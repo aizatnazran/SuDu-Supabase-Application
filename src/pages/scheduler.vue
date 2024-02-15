a19ea9eba5c73462af49cc7e62651361c07804dc
aizatnazran <aizatnazran@gmail.com>
2024-02-15 14:13:40 +0800

    15/2 scheduler page modification

--- src/pages/scheduler.vue
+++ src/pages/scheduler.vue
@@ -3,6 +3,7 @@ import { Background } from '@vue-flow/background'
 import { Controls } from '@vue-flow/controls'
 import { Panel, VueFlow, useVueFlow } from '@vue-flow/core'
 import { MiniMap } from '@vue-flow/minimap'
+import axios from 'axios'
 import { onMounted, ref } from 'vue'
 import { supabase } from '../lib/supaBaseClient.js'
 import ContactNode from './ContactNode.vue'
@@ -12,6 +13,9 @@ import SelectDateDailyNode from './SelectDateDailyNode.vue'
 import SelectDateWeeklyNode from './SelectDateWeeklyNode.vue'
 import UseCaseNode from './UseCaseNode.vue'
 const dialog = ref(false)
+const dialogs = ref({
+  questions: false,
+})
 const templateOptions = ref([])
 const selectedTemplate = ref(null)
 const selectedPhoneNumber = ref(null)
@@ -19,6 +23,20 @@ const selectedDays = ref([])
 const phoneNumbers = ref([])
 const userUUID = localStorage.getItem('uuid')
 const companyId = localStorage.getItem('company_id')
+const schedulers = ref([])
+
+const apiClient = axios.create({
+  baseURL: `http://sudu.ai:3002`,
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+const openQuestionsDialog = () => {
+  dialogs.value.questions = true
+}
 
 const { onConnect, addEdges } = useVueFlow()
 
@@ -27,6 +45,15 @@ const nodes = ref([{ id: '1', type: 'custom', label: 'Node 1', position: { x: 30
 onConnect(params => {
   addEdges([params])
 })
+
+async function fetchSchedulers() {
+  try {
+    const response = await apiClient.get('/all')
+    schedulers.value = response.data
+  } catch (error) {
+    console.error('Error fetching schedulers:', error)
+  }
+}
 
 async function fetchPhoneNumbers() {
   try {
@@ -84,6 +111,7 @@ function saveScheduler() {}
 
 onMounted(async () => {
   try {
+    await fetchSchedulers()
     templateOptions.value = await fetchTemplates()
     phoneNumbers.value = await fetchPhoneNumbers()
   } catch (error) {
@@ -160,18 +188,138 @@ onMounted(async () => {
     </div>
   </VCard>
 
-  <VRow v-else>
-    <VCardTitle class="font-weight-bold">Scheduler</VCardTitle>
-    <div class="d-flex flex-wrap gap-4">
+  <div v-else>
+    <VRow>
+      <VCardTitle class="font-weight-bold">Scheduler</VCardTitle>
       <VBtn
         @click="dialog = true"
-        class="rounded-pill"
+        class="rounded-pill mb-3"
       >
         <VIcon left>mdi-plus</VIcon>
         Add New
       </VBtn>
-    </div>
-  </VRow>
+    </VRow>
+    <VContainer>
+      <VRow>
+        <VCol
+          cols="12"
+          sm="6"
+          md="4"
+          v-for="scheduler in schedulers"
+          :key="scheduler.id"
+          class="py-2"
+        >
+          <VCard class="d-flex flex-column pa-4">
+            <div class="d-flex justify-space-between">
+              <div class="text-h6">{{ scheduler.question }}</div>
+              <VSwitch v-model="scheduler.isActive" />
+            </div>
+            <h6>Every Tuesday, Thursday</h6>
+            <div class="flex-grow-1 mb-5"></div>
+            <!-- Spacer div -->
+            <div class="text-caption">{{ scheduler.contact_number }}</div>
+            <div
+              class="text-primary text--underline cursor-pointer mt-1"
+              @click="openQuestionsDialog"
+            >
+              Questions
+            </div>
+          </VCard>
+        </VCol>
+      </VRow>
+    </VContainer>
+  </div>
+  <VDialog
+    v-model="dialogs.questions"
+    max-width="600px"
+    @click:outside="dialog = false"
+  >
+    <VCard>
+      <VCardText>
+        <VContainer>
+          <VRow>
+            <VCol
+              cols="12"
+              md="6"
+            >
+              <div class="text-h6 text-start mb-2 font-weight-bold">Use Case</div>
+              <VSelect
+                v-model="selectedTemplate"
+                :items="templateOptions"
+                label="Use Case"
+                item-text="name"
+                item-value="id"
+                return-object
+              ></VSelect>
+            </VCol>
+            <VCol
+              cols="12"
+              md="6"
+            >
+              <div class="text-h6 text-start font-weight-bold">Phone Number</div>
+              <VSelect
+                v-model="selectedPhoneNumber"
+                :items="phoneNumbers"
+                label="Phone Number"
+                item-text="number"
+                item-value="id"
+                return-object
+              ></VSelect>
+            </VCol>
+          </VRow>
+          <VRow>
+            <VCol>
+              <div class="text-h6 text-start mt-4 font-weight-bold">Selected Questions</div>
+              <div>1. What is my weekly average sales?</div>
+              <div>2. Which day has the highest sales?</div>
+            </VCol>
+          </VRow>
+          <VRow>
+            <VCol>
+              <div class="text-h6 text-start mb-5 font-weight-bold">Repeat</div>
+              <VRow>
+                <VBtnToggle v-model="selectedFrequency">
+                  <VBtn
+                    value="daily"
+                    class="primary"
+                    >Daily</VBtn
+                  >
+                  <VBtn value="weekly">Weekly</VBtn>
+                  <VBtn value="monthly">Monthly</VBtn>
+                  <VBtn value="custom">Custom</VBtn>
+                </VBtnToggle>
+              </VRow>
+              <VBtnToggle
+                v-model="selectedDays"
+                multiple
+              >
+                <VBtn value="Mon">Mon</VBtn>
+                <VBtn value="Tue">Tue</VBtn>
+                <VBtn value="Wed">Wed</VBtn>
+                <VBtn value="Thu">Thu</VBtn>
+                <VBtn value="Fri">Fri</VBtn>
+                <VBtn value="Sat">Sat</VBtn>
+                <VBtn value="Sun">Sun</VBtn>
+              </VBtnToggle>
+            </VCol>
+          </VRow>
+        </VContainer>
+      </VCardText>
+      <VCardActions>
+        <VSpacer></VSpacer>
+        <VBtn
+          @click="saveScheduler"
+          class="primary rounded-pill"
+          >Edit</VBtn
+        >
+        <VBtn
+          @click="saveScheduler"
+          class="primary rounded-pill"
+          >Save</VBtn
+        >
+      </VCardActions>
+    </VCard>
+  </VDialog>
 </template>
 
 <style>
@@ -182,4 +330,12 @@ onMounted(async () => {
 
 @import '@vue-flow/controls/dist/style.css';
 @import '@vue-flow/minimap/dist/style.css';
+
+.cursor-pointer {
+  cursor: pointer;
+}
+
+.text--underline {
+  text-decoration: underline;
+}
 </style>
