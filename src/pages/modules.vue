38671c92af452c05128dd8f67866735f2f561c6a
aizatnazran <aizatnazran@gmail.com>
2024-03-06 10:26:37 +0800

    6/3 Modules in cards

--- src/pages/modules.vue
+++ src/pages/modules.vue
@@ -3,89 +3,97 @@ import axios from 'axios'
 import FormData from 'form-data'
 import Swal from 'sweetalert2'
 import { ref } from 'vue'
-const modulePath = ref('')
-const handleUploadSuccess = moduleName => {
-  modulePath.value = `http://localhost:3000/modules/${moduleName}/`
-}
-const userUUID = localStorage.getItem('uuid')
-const companyId = localStorage.getItem('company_id')
+
+const modulePaths = ref([])
+const selectedModule = ref(null)
 const sheet = ref(false)
-//Function to upload module
+
 const uploadFiles = async file => {
   const formData = new FormData()
   formData.append('module', file)
 
   try {
-    // Send the request to your server endpoint
     const response = await axios.post('http://localhost:3000/upload-module', formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
     })
-    handleUploadSuccess(file.name.replace('.zip', ''))
+    modulePaths.value.push(`http://localhost:3000/modules/${file.name.replace('.zip', '')}/`)
     Swal.fire('Success', 'Module uploaded successfully!', 'success')
   } catch (error) {
     Swal.fire('Error', 'Failed to upload the module.', 'error')
   }
 }
 
-// You'll need to pass the selected file to the uploadFiles function
 const handleFileChange = event => {
-  const file = event.target.files[0] // Assuming single file upload
+  const file = event.target.files[0]
   if (file) {
     uploadFiles(file)
   }
 }
+
+const selectModule = module => {
+  selectedModule.value = module
+}
 </script>
 
 <template>
-  <VRow>
-    <VSpacer />
-    <VBtn
-      color="primary"
-      v-bind="props"
-      @click="sheet = !sheet"
-    >
-      Upload Document
-    </VBtn>
-  </VRow>
-  <iframe
-    v-if="modulePath"
-    :src="modulePath"
-    frameborder="0"
-    style="width: 100%; height: 600px"
-  ></iframe>
   <div>
-    <div class="text-center">
-      <VDialog
-        v-model="sheet"
-        activator="parent"
-        width="60%"
-        class="overlaying-component-class"
+    <VRow>
+      <VSpacer />
+      <VBtn
+        color="primary"
+        v-bind="props"
+        @click="sheet = !sheet"
       >
-        <VCard class="pa-5">
-          <VContainer>
-            <div class="text-h6 text-start font-weight-bold">Upload File</div>
-            <VFileInput
-              @click:input.stop
-              @change="handleFileChange"
-              label="Click to upload file or drag and drop here"
-              class="file-upload-input"
-            />
-          </VContainer>
-
-          <VBtn
-            prepend-icon="mdi-send-variant"
-            @click="uploadFiles"
-          >
-            Upload
-          </VBtn>
+        Upload Document
+      </VBtn>
+    </VRow>
+    <VRow>
+      <VCol
+        v-for="(module, index) in modulePaths"
+        :key="index"
+      >
+        <VCard @click="selectModule(module)">
+          <VCardText>{{ module }}</VCardText>
         </VCard>
-      </VDialog>
+      </VCol>
+    </VRow>
+    <iframe
+      v-if="selectedModule"
+      :src="selectedModule"
+      frameborder="0"
+      style="width: 100%; height: 600px"
+    ></iframe>
+    <div>
+      <div class="text-center">
+        <VDialog
+          v-model="sheet"
+          activator="parent"
+          width="60%"
+          class="overlaying-component-class"
+        >
+          <VCard class="pa-5">
+            <VContainer>
+              <div class="text-h6 text-start font-weight-bold">Upload File</div>
+              <VFileInput
+                @click:input.stop
+                @change="handleFileChange"
+                label="Click to upload file or drag and drop here"
+                class="file-upload-input"
+              />
+            </VContainer>
+            <VBtn
+              prepend-icon="mdi-send-variant"
+              @click="uploadFiles"
+              >Upload</VBtn
+            >
+          </VCard>
+        </VDialog>
+      </div>
     </div>
   </div>
 </template>
-
 <style>
 .high-z-index-swal {
   z-index: 3000 !important;
